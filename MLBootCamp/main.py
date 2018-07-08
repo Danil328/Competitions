556abcb24ad83f7e554a2764a6f957f15e38337e
Danil <Danil328@yandex.ru>
2018-07-08 20:19:20 +0500

    Add files via upload

--- MLBootCamp/main.py
+++ MLBootCamp/main.py
@@ -1,40 +1,151 @@
 import pandas as pd
-import numpy as np
-import seaborn as sns
 import os
 from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
-from sklearn.preprocessing import OneHotEncoder, StandardScaler, Normalizer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, Normalizer, MinMaxScaler
 from sklearn.metrics import roc_auc_score
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-from scipy.sparse import csr_matrix
-from scipy.sparse import hstack, vstack
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, SGDClassifier
 
-from tools import base_preprocassing, dict_vect_preprocessing, tfidf_preprocessing
+from tools import base_preprocassing, dict_vect_preprocessing, tfidf_preprocessing, split_train_test
 
-
+ACTION = 'train'
 PATH = './data/'
 
-#read data
-df = pd.read_csv(os.path.join(PATH, 'train_sample.csv'), nrows=400000, skiprows=range(1,200000))
-df = df.join(pd.read_csv(os.path.join(PATH,'mlboot_train_answers.tsv'), delimiter='\t').set_index('cuid'), on='cuid', how='inner')
+if ACTION=='train':
 
-#preprocess
-df = dict_vect_preprocessing(df)
-df = df.convert_objects(convert_numeric=True)
+    #read data
+    # df = pd.read_csv(os.path.join(PATH, 'train_sample.csv'), nrows=1000000)
+    # df = df.join(pd.read_csv(os.path.join(PATH,'mlboot_train_answers.tsv'), delimiter='\t').set_index('cuid'), on='cuid', how='inner')
 
-#try
-X_train, X_test, y_train, y_test = train_test_split(df.drop(axis=1, columns=['cuid','target']),df.target, test_size=0.2, random_state=17)
+    df = pd.read_csv(os.path.join(PATH,'mlboot_data.tsv'), delimiter='\t', nrows=3000000, header=None)
+    df.columns = ['cuid', 'cat', 'cnt1','cnt2','cnt3','data_diff']
+    df = df.join(pd.read_csv(os.path.join(PATH,'mlboot_train_answers.tsv'), delimiter='\t').set_index('cuid'), on='cuid', how='inner')
 
-n = Normalizer()
-X_train = n.fit_transform(X_train)
-X_test = n.transform(X_test)
+    #preprocess
+    df = dict_vect_preprocessing(df)
+    df = df.convert_objects(convert_numeric=True)
 
-lr = LogisticRegression(penalty='l2', C=1)
-sf = StratifiedKFold(n_splits=5, shuffle=True, random_state=17)
-cv = cross_val_score(lr,X_train, y_train, scoring='roc_auc', cv=sf, n_jobs=-1)
-print (cv.mean(), cv.std())
-lr.fit(X_train,y_train)
-pred = lr.predict_proba(X_test)
-print (roc_auc_score(y_test,pred[:, 1]))
+    #try
+    X_train, X_test, y_train, y_test = train_test_split(df.drop(axis=1, columns=['cuid','target']),df.target, test_size=0.2, random_state=17)
+
+    n = Normalizer()
+    X_train = n.fit_transform(X_train)
+    X_test = n.transform(X_test)
+
+    lr = LogisticRegression(penalty='l2', C=0.2,solver='lbfgs')
+    lr = SGDClassifier()
+    sf = StratifiedKFold(n_splits=5, shuffle=True, random_state=17)
+    cv = cross_val_score(lr,X_train, y_train, scoring='roc_auc', cv=sf, n_jobs=-1)
+    print (cv.mean(), cv.std())
+    lr.fit(X_train,y_train)
+    pred = lr.predict_proba(X_test)
+    print (roc_auc_score(y_test,pred[:, 1]))
+
+    # from evolutionary_search import EvolutionaryAlgorithmSearchCV
+    #
+    # paramgrid = {"solver": ["liblinear", 'lbfgs'],
+    #              "C"     : np.arange(0.1,1,0.1),
+    #              "penalty" : ['l2']}
+    #
+    # cv = EvolutionaryAlgorithmSearchCV(estimator=LogisticRegression(),
+    #                                    params=paramgrid,
+    #                                    scoring="roc_auc",
+    #                                    cv=StratifiedKFold(n_splits=5),
+    #                                    verbose=1,
+    #                                    population_size=50,
+    #                                    gene_mutation_prob=0.10,
+    #                                    gene_crossover_prob=0.5,
+    #                                    tournament_size=3,
+    #                                    generations_number=5,
+    #                                    n_jobs=4)
+    # cv.fit(X_train, y_train)
+    # Best individual is: {'solver': 'lbfgs', 'C': 0.2, 'penalty': 'l2'}
+    # with fitness: 0.6307805995054357
+
+    #SGDClassifier
+elif ACTION == 'predict':
+    train_ans = pd.read_csv(os.path.join(PATH,'mlboot_train_answers.tsv'), delimiter='\t')
+    test_id = pd.read_csv(os.path.join(PATH,'mlboot_test.tsv'), delimiter='\t')
+
+    step = 3100000 #19528597
+    for part, nrow in tqdm(enumerate(range(0,19528597,step))):
+        df = pd.read_csv(os.path.join(PATH,'mlboot_data.tsv'), delimiter='\t',nrows=step, skiprows=range(0,nrow),header=None)
+        df.columns = ['cuid', 'cat', 'cnt1','cnt2','cnt3','data_diff']
+
+        train_df = df.join(train_ans.set_index('cuid'), on='cuid', how='inner')
+        test_df = df.join(test_id.set_index('cuid'), on='cuid', how='inner')
+
+        train_df = dict_vect_preprocessing(train_df)
+        train_df = train_df.convert_objects(convert_numeric=True)
+
+        test_df = dict_vect_preprocessing(test_df, is_train=False)
+        test_df = test_df.convert_objects(convert_numeric=True)
+
+        X_train = train_df.drop(axis=1, columns=['cuid','target'])
+        y_train = train_df['target']
+        del train_df
+
+        X_test = test_df.drop(axis=1, columns=['cuid'])
+        test_id_part = test_df['cuid']
+        del test_df
+
+        n = Normalizer()
+        X_train = n.fit_transform(X_train)
+        X_test = n.transform(X_test)
+
+        lr = LogisticRegression(penalty='l2', C=0.2,solver='lbfgs')
+        lr.fit(X_train,y_train)
+
+        scaler = MinMaxScaler()
+        ans_df = pd.DataFrame(test_id_part, columns=['cuid'])
+        ans_df['target'] = scaler.fit_transform(lr.predict_proba(X_test)[:, 1].reshape(-1,1))
+
+        ans_df.to_csv(os.path.join('submission','submission_part{}.csv'.format(part)), index=False)
+elif ACTION=='union':
+    test_id = pd.read_csv(os.path.join(PATH,'mlboot_test.tsv'), delimiter='\t')
+    union_df = pd.DataFrame(columns=['cuid','target'])
+    path = './submission'
+    files = os.listdir(path)
+    for file in files:
+        if 'part' in file:
+            df = pd.read_csv(os.path.join(path,file))
+            union_df=pd.concat([union_df,df[['cuid','target']]],axis=0)
+    union_df = union_df.groupby(['cuid'])['target'].mean()
+    predicts = test_id.set_index('cuid').join(union_df, on='cuid', how='inner')
+    predicts.to_csv(os.path.join(path,'union.csv'), index=False, header=False)
+
+#split_train_test(PATH)
+
+#TRAIN
+#test_id = pd.read_csv(os.path.join(PATH,'mlboot_test.tsv'), delimiter='\t')
+
+# step = 3000000
+# lr = LogisticRegression(penalty='l2', C=0.2,solver='lbfgs', warm_start=True)
+# n = Normalizer()
+#
+# for part, nrow in tqdm(enumerate(range(1,12874345,step))):
+#     df = pd.read_csv(os.path.join(PATH,'train.csv'), nrows=step, skiprows=range(1,nrow))
+#
+#     df = dict_vect_preprocessing(df)
+#     df = df.convert_objects(convert_numeric=True)
+#
+#     lr.fit(n.fit_transform(df.drop(axis=1, columns=['cuid','target'])), df['target'])
+#
+#
+# import  pickle
+# # save the model to disk
+# filename = 'lr.sav'
+# pickle.dump(lr, open(filename, 'wb'))
+#
+# # some time later...
+#
+# # load the model from disk
+# loaded_model = pickle.load(open(filename, 'rb'))
+# result = loaded_model.score(X_test, Y_test)
+# print(result)
+#
+# df = pd.read_csv(os.path.join(PATH,'test.csv'))
+#
+# df = dict_vect_preprocessing(df,is_train=False)
+# df = df.convert_objects(convert_numeric=True)
--- MLBootCamp/tools.py
+++ MLBootCamp/tools.py
@@ -5,6 +5,12 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.decomposition import TruncatedSVD
 
+import gc
+
+from tqdm import tqdm
+
+gc.enable()
+
 def base_preprocassing(df):
     #Заполненность счетчиков
     df['cnt1_is_not_null'] = df['cnt1'].apply(lambda x: 0 if x == "{}" else 1)
@@ -51,11 +57,15 @@ def base_preprocassing(df):
 
     return df_base
 
-def dict_vect_preprocessing(df):
+def dict_vect_preprocessing(df, is_train=True):
     df = df.sort_values(['data_diff'], ascending = True)
     df_dict = pd.DataFrame()
     df_dict['cuid'] = df.groupby(['cuid'])['cuid'].min()
-    df_dict['target'] = df.groupby(['cuid'])['target'].min()
+    if is_train:
+        df_dict['target'] = df.groupby(['cuid'])['target'].min()
+        cols = ['cuid','target']
+    else:
+        cols = ['cuid']
 
     df['cnt1'] = df['cnt1'].apply(lambda x: x[1:-1]+',' if len(x)>2 else '')
     df['cnt2'] = df['cnt2'].apply(lambda x: x[1:-1]+',' if len(x)>2 else '')
@@ -80,13 +90,20 @@ def dict_vect_preprocessing(df):
     dv_cnt2 = svd.fit_transform(dv_cnt2)
     dv_cnt3 = svd.fit_transform(dv_cnt3)
 
-    return pd.DataFrame(np.hstack([df_dict[['cuid','target']].values.reshape(-1,2),dv_cnt1,dv_cnt2,dv_cnt3]), columns=['cuid','target']+list(range(dv_cnt1.shape[1]*3)))
+    return pd.DataFrame(np.hstack([df_dict[cols].values.reshape(-1,len(cols)),dv_cnt1,dv_cnt2,dv_cnt3]), columns=cols+list(range(dv_cnt1.shape[1]*3)))
 
-def tfidf_preprocessing(df):
+def tfidf_preprocessing(df, is_train=True):
     #Номер счетчика + значение
     df = df.sort_values(['data_diff'], ascending = True)
     df_tfidf = pd.DataFrame()
     df_tfidf['cuid'] = df.groupby(['cuid'])['cuid'].min()
+
+    if is_train:
+        df_tfidf['target'] = df.groupby(['cuid'])['target'].min()
+        cols = ['cuid','target']
+    else:
+        cols = ['cuid']
+
     df_tfidf['target'] = df.groupby(['cuid'])['target'].min()
 
     df['cnt1'] = df['cnt1'].apply(lambda x: x[1:-1])
@@ -100,13 +117,37 @@ def tfidf_preprocessing(df):
     new_df_cnt2 = df.groupby(['cuid'])['cnt2'].sum()
     new_df_cnt3 = df.groupby(['cuid'])['cnt3'].sum()
 
-    tf_idf_cnt1 = tf_idf.fit_transform(new_df_cnt1)
-    tf_idf_cnt2 = tf_idf.fit_transform(new_df_cnt2)
-    tf_idf_cnt3 = tf_idf.fit_transform(new_df_cnt3)
+    new_df_cnt1 = tf_idf.fit_transform(new_df_cnt1)
+    new_df_cnt2 = tf_idf.fit_transform(new_df_cnt2)
+    new_df_cnt3 = tf_idf.fit_transform(new_df_cnt3)
 
     svd = TruncatedSVD(n_components=150, random_state=17, n_iter=5)
-    tf_idf_cnt1 = svd.fit_transform(tf_idf_cnt1)
-    tf_idf_cnt2 = svd.fit_transform(tf_idf_cnt2)
-    #tf_idf_cnt3 = svd.fit_transform(tf_idf_cnt3)
+    tf_idf_cnt1 = svd.fit_transform(new_df_cnt1)
+    tf_idf_cnt2 = svd.fit_transform(new_df_cnt2)
+    #tf_idf_cnt3 = svd.fit_transform(new_df_cnt3)
 
-    return pd.DataFrame(np.hstack([df_tfidf[['cuid','target']].values.reshape(-1,2),tf_idf_cnt1,tf_idf_cnt2]), columns=['cuid','target']+list(range(tf_idf_cnt1.shape[1]*2)))
+    return pd.DataFrame(np.hstack([df_tfidf[cols].values.reshape(-1,len(cols)),tf_idf_cnt1,tf_idf_cnt2]), columns=cols+list(range(tf_idf_cnt1.shape[1]*2)))
+
+import os
+def split_train_test(PATH):
+    #split train and test
+    train_ans = pd.read_csv(os.path.join(PATH,'mlboot_train_answers.tsv'), delimiter='\t')
+    #test_id = pd.read_csv(os.path.join(PATH,'mlboot_test.tsv'), delimiter='\t')
+    step = 3000000
+
+    #test_all = pd.DataFrame(columns = ['cuid', 'cat', 'cnt1','cnt2','cnt3','data_diff'])
+    train_all = pd.DataFrame(columns = ['cuid', 'cat', 'cnt1','cnt2','cnt3','data_diff', 'target'])
+    for part, nrow in tqdm(enumerate(range(0,19528597,step))):
+        df = pd.read_csv(os.path.join(PATH,'mlboot_data.tsv'), delimiter='\t',nrows=step, skiprows=range(0,nrow),header=None)
+        df.columns = ['cuid', 'cat', 'cnt1','cnt2','cnt3','data_diff']
+
+        #test_df = df.join(test_id.set_index('cuid'), on='cuid', how='inner')
+        train_df = df.join(train_ans.set_index('cuid'), on='cuid', how='inner')
+
+        #test_all = pd.concat([test_all,test_df])
+        train_all = pd.concat([train_all,train_df])
+
+    #test_all.to_csv(os.path.join(PATH,'test.csv'),index=False)
+    train_all.to_csv(os.path.join(PATH,'train.csv'),index=False)
+
+
